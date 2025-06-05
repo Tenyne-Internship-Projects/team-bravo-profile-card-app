68a60927932ac6988232b59c1178d21973197e91
Keys-digital <eseogheneikoyo23@gmail.com>
2025-06-05 16:13:20 +0100

    Add jest.config.js debug log and update test script

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,7 @@
+console.log("jest.config.js loaded");
+
 module.exports = {
   testMatch: ["**/tests/**/*.test.js"],
   testPathIgnorePatterns: ["/node_modules/"],
-  // optional if you want verbose logs
   verbose: true,
 };
